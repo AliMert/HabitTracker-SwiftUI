6aa17d12ba2d38b52c72155f9793ea024a9f2688
Ali Mert Özhayta <mozhayta@hotmail.com>
2022-05-19 17:15:18 +0300

    add Core Data entity and attributes

---
+++ Shared/App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Ali Mert Özhayta on 19.05.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+
+    var body: some View {
+        Text("Hello")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
